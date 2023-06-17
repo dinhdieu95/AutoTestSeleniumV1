e8e33a61746d957bc86204e5aa5ddf15f0fc63bc
ASUS_ntyen <nguyenyen1508@gmail.com>
2023-06-17 11:47:09 +0700

    yen commit b3.bai1

--- src/main/java/giaibai/b3/Bai1.java
+++ src/main/java/giaibai/b3/Bai1.java
@@ -2,15 +2,27 @@ package giaibai.b3;
 
 public class Bai1 {
     public static void main(String[] args) {
-//        int [] num = {1,4,2,3,0};
-//        for (int i = 0; i < num.length, i++){
-//            int min_index = i;
-//            for (int j = i+1, j < num.length, J++){
-//
-//            }
-//
-//        }
-        System.out.println("hello");
+        int [] num = {1,4,2,3,0};
+        System.out.println("Cho mang so nguyen: ");
+        for (int i = 0 ; i<num.length ; i++){
+            System.out.println(num[i]);
+        }
+        for (int i = 0; i < num.length; i++){
+            int max_index = num[i];
+            for (int j = i + 1 ; j< num.length ; j++){
+                if (num[j]> max_index) {
+                    num [i] = num [j];
+                    num[j] = max_index;
+                    max_index = num [i];
+                }
+
+            }
+        }
+        System.out.println("mang duoc sap xep theo thu tu giam dan la: ");
+        for (int x: num){
+            System.out.println(x);
+        }
+
     }
 
 
