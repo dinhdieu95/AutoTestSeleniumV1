3465f25e06974f3097d00925ad484b6acf4592cb
ASUS_ntyen <nguyenyen1508@gmail.com>
2023-06-17 11:11:07 +0700

    yen test commit git

---
+++ src/main/java/giaibai/b3/Bai1.java
@@ -0,0 +1,16 @@
+package giaibai.b3;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        int [] num = {1,4,2,3,0};
+        for (int i = 0; i < num.length, i++){
+            int min_index = i;
+            for (int j = i+1, j < num.length, J++){
+
+            }
+
+        }
+    }
+
+
+}
