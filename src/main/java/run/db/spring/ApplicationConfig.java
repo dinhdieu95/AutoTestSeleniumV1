8d6e998380e5b6ae427daf1e9c25f86381637fdc
dieudta <dieudta@tcbs.com.vn>
2023-07-09 17:53:02 +0700

    fix

--- src/main/java/run/db/spring/ApplicationConfig.java
+++ src/main/java/run/db/spring/ApplicationConfig.java
@@ -1,13 +1,18 @@
 package run.db.spring;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource(value={"classpath:application.properties"})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ApplicationConfig {
 
     @Value("${spring.datasource.url}")
