867dc1d2c512038eff816974d35056fb4c07d1cc
dieudta <dinhthianhdieu@gmail.com>
2023-07-23 13:14:49 +0700

    add example

--- src/main/java/selenium/common/Common.java
+++ src/main/java/selenium/common/Common.java
@@ -8,8 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Common {
     //    public SOURCE_URL = System.getProperty("user.dir") + "/src/test/resources/";
     public static final String URL_SRC_CHROME = System.getProperty("user.dir") + "/src/test/resources/driver/chromedriver.exe";
+    public static final String URL_SRC_FIREFOX = System.getProperty("user.dir") + "/src/test/resources/driver/geckodriver.exe";
+    public static final String URL_SRC_ME = System.getProperty("user.dir") + "/src/test/resources/driver/msedgedriver.exe";
     public static final String CONFIG_DRIVER_CHROME = "webdriver.chrome.driver";
     public static final String CONFIG_DRIVER_FIREFOX = "webdriver.gecko.driver";
+    public static final String CONFIG_DRIVER_ME = "webdriver.edge.driver";
 
     public static WebDriverManager driver = WebDriverManager.chromedriver();
     public static WebDriver drv;
---
+++ src/test/java/selenium/TakeScreenTest.java
@@ -0,0 +1,44 @@
+package selenium;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static selenium.common.Common.CONFIG_DRIVER_CHROME;
+import static selenium.common.Common.URL_SRC_CHROME;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TakeScreenTest {
+    WebDriver driver;
+
+    @Test
+    public void takeScreenTest() {
+        System.setProperty(CONFIG_DRIVER_CHROME, URL_SRC_CHROME);
+        driver = new ChromeDriver();
+
+        // Alert Message handling
+
+        driver.get("https://demo.guru99.com/test/delete_customer.php");
+        // Tạo đối tượng TakesScreenshot từ đối tượng WebDriver để chụp ảnh màn hình
+        TakesScreenshot scrShot = ((TakesScreenshot) driver);
+
+        // Gọi phương thức getScreenshotAs để tạo tệp ảnh
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+        // Di chuyển tệp ảnh đến đích mới
+        File DestFile = new File( System.getProperty("user.dir") + "/src/test/resources/driver/testTakeScreen.png");
+
+        // Sao chép tệp vào đích
+        try {
+            FileUtils.copyFile(SrcFile, DestFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        driver.close();
+    }
+}
