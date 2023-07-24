5a3d0a14ecb0d7c83043c96f8ee7198e1607e441
dieudta <dinhthianhdieu@gmail.com>
2023-07-24 21:18:48 +0700

    add comment

--- src/test/java/selenium/ProxyTest.java
+++ src/test/java/selenium/ProxyTest.java
@@ -1,7 +1,5 @@
 package selenium;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
