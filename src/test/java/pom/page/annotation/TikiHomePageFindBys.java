681d7c00a46d72dc389382b2454dc8a659ae6ecd
dieudta <dinhthianhdieu@gmail.com>
2023-07-31 23:11:10 +0700

    add result test

---
+++ src/test/java/pom/page/annotation/TikiHomePageFindBys.java
@@ -0,0 +1,32 @@
+package pom.page.annotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+public class TikiHomePageFindBys {
+    private WebDriver driver;
+
+    @FindBys({
+            @FindBy(xpath = "//*[@data-view-id='main_search_form_input']"),
+            @FindBy(xpath = "//*[@data-view-id='main_search_form_button']")
+    })
+    private List<WebElement> searchInput;
+
+    public TikiHomePageFindBys(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void searchForProduct(String keyword) {
+        if (!searchInput.isEmpty()) {
+            searchInput.get(0).sendKeys(keyword);
+            searchInput.get(0).click();
+        } else {
+            System.out.println("Search input not found.");
+        }
+    }
+}
