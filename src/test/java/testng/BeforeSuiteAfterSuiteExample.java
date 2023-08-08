73cfe484c950cec912d8a0ef59550d3008107ee6
dieudta <dinhthianhdieu@gmail.com>
2023-08-08 23:18:44 +0700

    ví dụ testng

---
+++ src/test/java/testng/BeforeSuiteAfterSuiteExample.java
@@ -0,0 +1,38 @@
+package testng;
+import org.testng.annotations.*;
+public class BeforeSuiteAfterSuiteExample {
+    @BeforeSuite(alwaysRun = true, dependsOnGroups = {"setup"})
+    public void setupSuite() {
+        System.out.println("Before Suite: Setting up suite resources");
+    }
+
+    @Test(groups = "setup")
+    public void setupGroup() {
+        System.out.println("Test Group: Setup resources for the tests");
+    }
+
+    @BeforeSuite(alwaysRun = true, dependsOnMethods = {"setupSuite"})
+    public void beforeSuite() {
+        System.out.println("Before Suite: Running common setup tasks");
+    }
+
+    @Test(groups = {"group1"})
+    public void testCase1() {
+        System.out.println("Test Case 1");
+    }
+
+    @Test(groups = {"group2"})
+    public void testCase2() {
+        System.out.println("Test Case 2");
+    }
+
+    @Test(groups = {"group1", "group2"})
+    public void testCase3() {
+        System.out.println("Test Case 3");
+    }
+
+    @BeforeSuite(enabled = false)
+    public void skippedSuite() {
+        System.out.println("This suite won't run");
+    }
+}
---
+++ src/test/java/testng/TestParallel.java
@@ -0,0 +1,16 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class TestParallel {
+    @Test
+    public void method1() {
+        System.out.println("Inside method1()");
+        //Assert.assertEquals(message, messageUtil.printMessage());
+    }
+    @Test
+    public void method2() {
+        System.out.println("Inside method2()");
+        //Assert.assertEquals(message, messageUtil.printMessage());
+    }
+}
